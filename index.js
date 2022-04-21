89e847243b9f6e806a481583bad139f3dccb7c79
qalakp <104077927+qalakp@users.noreply.github.com>
2022-04-21 18:13:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('message', message => {
 
 if (!message.guild) return; 
 
-if (message.content === '') { 
+if (message.content === 'tuxwa wara') { 
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(``) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(`ڕیکلام کرا تۆش ڕیکلام بکەhttps://discord.gg/6HaDAv6S3B`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("").send(
+  client.channels.get("955867903893315640").send(
 `> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(``,"http://twitch.tv/PAYWANDcamper") 
+client.user.setGame(`ڕیکلام بنێرە بۆ من`,"http://twitch.tv/PAYWANDcamper") 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("OTY2NzE0NzYyMzkyMTMzNjUy.YmFxSA.ndrKFiTLWdKOHDnKkd4jizYslDg");//تۆکین لێرە دانێ
